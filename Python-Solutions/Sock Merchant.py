e8136ba32c8595baf138d8f68d6a2843b78684ce
Cleiton M. Marques <cleitonmarquees@gmail.com>
2020-08-11 11:24:44 -0300

    Initial commit

---
+++ Python-Solutions/Sock Merchant.py
@@ -0,0 +1,35 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    ar.sort()
+    countPars = 0
+    pars = []
+    for element in ar:
+        nElements = ar.count(element)
+        if (element not in pars):
+            pars.append(element)
+            if(nElements%2 == 0):
+                countPars += nElements//2
+            elif(((nElements-1)%2 == 0) and ((nElements-1) != 0)):
+                countPars += (nElements-1)//2
+    return countPars
+
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    print(result)
+    
+    #fptr.write(str(result) + '\n')
+
+    #fptr.close()
